e8ae7fc8ffd423c3891dfa07d775520b17825a8d
Stanislaw <92331225+tabiszs@users.noreply.github.com>
2024-06-20 13:13:30 +0200

    add robot positioning - first edition

--- Backend/README.md
+++ Backend/README.md
@@ -1,4 +1,45 @@
-# melman-vr-backend
+# Melman VR Backend
+
+Part of Melman VR project to move robot using VR controllers. This part should be run on robot OS [Ubuntu 22.04](https://releases.ubuntu.com/jammy/) with [ROS2 Humble](https://docs.ros.org/en/humble/index.html) framework. Commands 1-14 include ROS2 Humble installation
+
+## Enable Virtual Melman
+
+To run virtual Melman robot download [knr_ros2_humanoid_virtual_twin](https://github.com/KNR-PW/knr_ros2_humanoid_virtual_twin) repository and execute following commands in terminal
+``` bash
+1  locale  # check for UTF-8
+2  sudo apt update && sudo apt install locales
+3  sudo locale-gen en_US en_US.UTF-8
+4  sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+5  export LANG=en_US.UTF-8
+6  locale  # verify settings
+7  sudo apt install software-properties-common
+8  sudo add-apt-repository universe
+9  sudo apt update && sudo apt install curl -y
+10  sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+11  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+12  sudo apt update
+13  sudo apt upgrade
+14  sudo apt install ros-humble-desktop
+17  source /opt/ros/humble/setup.bash
+19  sudo apt install git
+20  mkdir Humanoid_workspace
+21  cd Humanoid_workspace/
+22  mv knr_ros2_humanoid_virtual_twin/ src/
+23  git clone https://github.com/KNR-PW/knr_ros2_humanoid_virtual_twin.git
+24  sudo apt install ros-humble-ros2-control ros-humble-ros2-controllers
+25  sudo apt install python3-colcon-common-extensions
+26  sudo apt install python3-rosdep
+27  sudo apt install ros-humble-gazebo-ros-pkgs
+28  sudo rosdep init
+29  rosdep update
+30  rosdep install -i --from-path src --rosdistro humble -y
+32  source /opt/ros/humble/setup.bash
+33  colcon build --packages-up-to ros2_humanoid_virtual_twin
+34  source /opt/ros/humble/setup.bash
+35  source install/local_setup.bash
+36  ros2 launch ros2_humanoid_virtual_twin rviz_launch.py 
+37  ros2 launch ros2_humanoid_virtual_twin gazebo_Melman_ros2control.launch.py
+```
 
 ## How to Run the Server
 
@@ -9,7 +50,7 @@
     pip install flask pydantic
     ```
 
-3. Save the script to a file, for example `server.py`.
+3. Save the script to a file, for example `main.py`.
 4. Run the server with the command:
 
     ```bash
@@ -19,6 +60,8 @@
 The server will be running on `http://X.Y.X.W:5000` and will accept POST requests at the `/frames` endpoint with a JSON body matching the `FrameDto` schema.
 
 ## Example JSON Payload
+
+This payload is posted from VR headset.
 
 Here's an example of what the JSON payload should look like when sending a POST request to the `/frames` endpoint:
 
@@ -36,14 +79,26 @@ Here's an example of what the JSON payload should look like when sending a POST 
         "position": {"x": 7.0, "y": 8.0, "z": 9.0},
         "rotation": {"x": 0.0, "y": 0.0, "z": 0.0}
     },
+    "forward": {"x": 0.0, "y": 0.0, "z": 1.0},
     "time": 123.456
 }
 ```
 
 You can use a tool like `curl` or Postman to send this payload to the server:
 
+1. T-pose
 ```bash
-curl -X POST http://127.0.0.1:5000/frames -H "Content-Type: application/json" -d '{"hmd":{"position":{"x":1.0,"y":2.0,"z":3.0},"rotation":{"x":0.0,"y":0.0,"z":0.0}},"leftHand":{"position":{"x":4.0,"y":5.0,"z":6.0},"rotation":{"x":10.0,"y":20.0,"z":30.0}},"rightHand":{"position":{"x":7.0,"y":8.0,"z":9.0},"rotation":{"x":40.0,"y":50.0,"z":60.0}},"time":123.456}'
+curl -X POST http://127.0.0.1:5000/frames -H "Content-Type: application/json" -d '{"hmd":{"position":{"x":0.07,"y":1.29,"z":0.07},"rotation":{"x":0.0,"y":0.0,"z":0.0}},"leftHand":{"position":{"x":-0.73,"y":1.15,"z":-0.10},"rotation":{"x":0.0,"y":0.0,"z":0.0}},"rightHand":{"position":{"x":0.84,"y":1.08,"z":0.53},"rotation":{"x":0.0,"y":0.0,"z":0.0}},"forward": {"x": 0.0, "y": 0.0, "z": 1.0},"time":1.00}'
 ```
 
-This will send the JSON data to the server, which will parse it into a `FrameDto` object and print it out.
+2. I-pose
+```bash
+curl -X POST http://127.0.0.1:5000/frames -H "Content-Type: application/json" -d '{"hmd":{"position":{"x":0.07,"y":1.29,"z":0.07},"rotation":{"x":0.0,"y":0.0,"z":0.0}},"leftHand":{"position":{"x":-0.43,"y":0.5,"z":-0.5},"rotation":{"x":0.0,"y":0.0,"z":0.0}},"rightHand":{"position":{"x":0.25,"y":0.5,"z":0.15},"rotation":{"x":0.0,"y":0.0,"z":0.0}},"forward": {"x": 0.0, "y": 0.0, "z": 1.0},"time":1.00}'
+```
+
+## TODO
+1. check the coordinate system: I used Y upwards
+2. determine which servo names correspond to head movements. 
+3. assign angles and run actions extended by head movements
+2. test in real life on the robot
+3. possibly speed up the frequency of sending actions. Currently set to 0.2x/second
--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,26 @@
 from flask import Flask, request, jsonify
 from pydantic import BaseModel
 from typing import Optional
+from threading import Thread
+from dataclasses import dataclass
+import numpy as np
+import subprocess
+import time
+import math
 import json
+
+# Define constants
+PI = math.pi
+PI_OVER_2 = math.pi / 2
+head_to_shoulder = 0.2
+forwardDistanceBetweenHandsAndHead = 0.1
+headHeight = 0.2
+tolerance = 0.1
+up_vector = np.array([0.0, 1.0, 0.0])
 
 app = Flask(__name__)
 
-start_tracking = False
+# Dto definitions
 
 class Vector3Dto(BaseModel):
     x: float
@@ -23,13 +38,34 @@ class FrameDto(BaseModel):
     rightHand: DeviceDto
     time: float
 
+# Server variable
+start_tracking = False
+arm_length = 0.5
+right_vector = np.array([0.0, 0.0, 0.0])
+neck_position = np.array([0.0, 0.0, 0.0])
+left_shoulder_position = np.array([0.0, 0.0, 0.0])
+right_shoulder_position = np.array([0.0, 0.0, 0.0])
+ra_y = 0
+ra_x = 0
+la_y = 0
+la_x = 0
+head_yaw = 0
+head_pitch = 0
+
 @app.route('/start-tracking', methods=['POST'])
 def start_tracing():
+    global start_tracking, arm_length, left_shoulder_position, right_shoulder_position
     try:
         data = request.json
         frame_dto = FrameDto(**data)
-        # Process initial position
-        # oblicz pozycje barkow i dlugosc rak
+        
+        # Set arm length
+        get_shoulders_position(frame_dto)
+        left_hand_position = np.array([frame_dto.leftHand.position.x, frame_dto.leftHand.position.y, frame_dto.leftHand.position.z])
+        lal = np.abs(left_hand_position, left_shoulder_position)
+        right_hand_position = np.array([frame_dto.rightHand.position.x, frame_dto.rightHand.position.y, frame_dto.rightHand.position.z])
+        ral = np.abs(right_hand_position - right_shoulder_position)
+        arm_length = np.mean(lal, ral)
 
         print(frame_dto)
         start_tracking = True;
@@ -39,17 +75,126 @@ def start_tracing():
 
 @app.route('/frames', methods=['POST'])
 def accept_frame():
+    global ra_y, ra_x, la_y, la_x
     try:
         data = request.json
-        frame_dto = FrameDto(**data)
-        # Set head rotation and send as action
-        # Compute angles in arms and send as action
+        frame_dto = FrameDto(**data)        
+        inverse_kinematics(frame_dto)
+        dest_time = 10  # Destination time in seconds      
 
-        print(frame_dto)
+        # Start ROS2 command in a separate thread
+        ros2_thread = Thread(target=run_ros2_command, args=(ra_y, ra_x, la_y, la_x, dest_time))
+        ros2_thread.start()
+
         return jsonify({"message": "Frame received successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+    
+def get_neck_position(frame_dto):
+    headPosition = np.array([frame_dto.hmd.position.x, frame_dto.hmd.position.y, frame_dto.hmd.position.z])
+    forwardVector = np.array([frame_dto.forward.x, frame_dto.forward.y, frame_dto.forward.z])
+    neck_position = headPosition - forwardVector * forwardDistanceBetweenHandsAndHead
+    neck_position[1] -= headHeight
+    return neck_position
+
+def get_shoulders_position(frame_dto):
+    global neck_position, right_vector, right_shoulder_position, left_shoulder_position
+    neck_position = get_neck_position(frame_dto)
+    #print("neck_position: " + str(neck_position))
+    forward = np.array([frame_dto.forward.x, frame_dto.forward.y, frame_dto.forward.z])
+    # TODO: sepecify proper coordinate system axis, 
+    # TODO: revert/change ra_x, ra_y, la_x, la_y to adjust to coordinate system
+    right_vector = +1 * np.cross(forward, up_vector) 
+    #print("right_vector: "+str(right_vector))
+    right_shoulder_position = neck_position + right_vector * head_to_shoulder
+    #print("right_shoulder_position: " + str(right_shoulder_position))
+    left_shoulder_position = neck_position - right_vector * head_to_shoulder  
+    #print("left_shoulder_position: " + str(left_shoulder_position))
+
+def normalization(direction_vector):
+    magnitude = np.linalg.norm(direction_vector)
+    normalized_vector = direction_vector / magnitude if magnitude != 0 else direction_vector
+    return normalized_vector
+
+def get_left_arm_angles(left_pos):
+    global la_x, la_y
+    left_hand_position = np.array([left_pos.x, left_pos.y, left_pos.z])
+    direction_vector = left_hand_position - left_shoulder_position
+    shoulder_to_hand_vector = normalization(direction_vector)
+    # XZ planar part of vector
+    xz_neg_right_vector = normalization(np.array([-right_vector[0], -right_vector[2]]))
+    xz_shoulder_to_hand_vector = normalization(np.array([shoulder_to_hand_vector[0], shoulder_to_hand_vector[2]]))
+    dot_product = np.dot(xz_neg_right_vector, xz_shoulder_to_hand_vector)
+    # Calculate the magnitudes
+    magnitude_xz_neg_right_vector = np.linalg.norm(xz_neg_right_vector)
+    magnitude_xz_normalized_vector = np.linalg.norm(xz_shoulder_to_hand_vector)        
+    # Calculate the angle in radians
+    la_y = math.acos(dot_product / (magnitude_xz_neg_right_vector * magnitude_xz_normalized_vector))
+    # Y Height part of vector
+    y_part = left_hand_position[1] - neck_position[1]
+    la_x = math.atan2(y_part, arm_length) - PI_OVER_2
+    
+def get_right_arm_angles(right_pos):
+    global ra_x, ra_y
+    right_hand_position = np.array([right_pos.x, right_pos.y, right_pos.z])
+    direction_vector = right_hand_position - right_shoulder_position
+    shoulder_to_hand_vector = normalization(direction_vector)
+    # XZ planar part of vector
+    xz_right_vector = normalization(np.array([right_vector[0], right_vector[2]]))
+    xz_shoulder_to_hand_vector = normalization(np.array([shoulder_to_hand_vector[0], shoulder_to_hand_vector[2]]))
+    dot_product = np.dot(xz_right_vector, xz_shoulder_to_hand_vector)
+    # Calculate the magnitudes
+    magnitude_xz_right_vector = np.linalg.norm(xz_right_vector)
+    magnitude_xz_normalized_vector = np.linalg.norm(xz_shoulder_to_hand_vector)        
+    # Calculate the angle in radians
+    ra_y = math.acos(dot_product / (magnitude_xz_right_vector * magnitude_xz_normalized_vector))
+    # Y Height part of vector
+    y_part = right_hand_position[1] - neck_position[1]
+    ra_x = math.atan2(y_part, arm_length) + PI_OVER_2
+
+def get_head_angles(hmd_rotation):
+    global head_yaw, head_pitch
+    head_pitch = hmd_rotation.y
+    head_yaw = hmd_rotation.z
+
+def inverse_kinematics(frame_dto):
+    get_shoulders_position(frame_dto)
+    get_left_arm_angles(frame_dto.leftHand.position)
+    get_right_arm_angles(frame_dto.rightHand.position)
+    get_head_angles(frame_dto.hmd.rotation)
+
+def run_ros2_tpose():
+    run_ros2_command(0, PI_OVER_2, 0, -PI_OVER_2, 10)
+
+def run_ros2_command(ra_y, ra_x, la_y, la_x, dest_time):
+    command = [
+        "ros2", "action", "send_goal", "/joint_trajectory_controller/follow_joint_trajectory",
+        "control_msgs/action/FollowJointTrajectory", "-f", 
+        f'''{{
+            "trajectory": {{
+                "joint_names": ["RTz", "RTx", "RTy", "RSy", "RFy", "RFx", "LTz", "LTx", "LTy", "LSy", "LFy", "LFx", "RAy", "RAx", "LAy", "LAx"],
+                "points": [
+                    {{
+                        "positions": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, {ra_y}, {ra_x}, {la_y}, {la_x}], 
+                        "time_from_start": {{"sec": {dest_time}}}
+                    }}
+                ]
+            }}
+        }}'''
+    ]
+    print(command)
+    proc = subprocess.Popen(command)
+    time.sleep(5*dest_time)  # Wait for the destination time # 5x for simulation
+    proc.terminate()  # Terminate the subprocess
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # Set robotin T-pose
+    # Start the server in a separate thread
+    server_thread = Thread(target=lambda: app.run(debug=True, use_reloader=False))
+    server_thread.start()
+    
+    # Give the server a moment to start up
+    #time.sleep(5)    
+    #run_ros2_tpose()
+    
+    # Join the server thread to keep the script running
+    server_thread.join()
